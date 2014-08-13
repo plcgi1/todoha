ee09faaf995a7eb882e0abf800b8b885e9fd23d7
Alex <plcgi1@gmail.com>
2014-08-13 17:48:23 +0300

    Update main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,7 @@
 ;(function(){
     // роутер для показа задач по состояниям
     var Router = Backbone.Router.extend({
+        
         routes : {
             "filter/:route" : "filter"
         },
@@ -12,6 +13,7 @@
             this.defaultRoute = 'waiting';
             Backbone.history.start();
         },
+        // главный фильтр - выводит таски только по статусу
         filter: function(route){
             if(location.hash){
                 route = location.hash.split('/')[1];
@@ -19,12 +21,17 @@
             else if(!route || typeof route == 'undefined' ){
                 route = this.defaultRoute;    
             }
+            // отрработка списка тасков по статусу
             this.views.status.render(route);
+            // установка пункта меню
             this.views.nav.set_active(route);
         }
     });
+    // главная модель для хранения тасков
     var tasks = window.models.Tasks.initialize();
+    // отображение тасков
     var status_view = window.views.List.initialize({ model : tasks });
+    // отображение фильтра тасков
     var nav_view = window.views.Nav.initialize();
     
     var router = new Router({
